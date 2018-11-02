c2948cc0aabb36b50b0e96c2e73238419109ab89
Fernando <ferry.fernando@gmail.com>
2018-11-02 15:46:09 +0700

    Product category filter.

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -8,13 +8,19 @@ import { ProductRepository } from '../model/product.repository';
   templateUrl: 'store.component.html'
 })
 export class StoreComponent {
+  public selectedCategory = null;
+
   constructor(private repository: ProductRepository) {}
 
   get products(): Product[] {
-    return this.repository.getProducts();
+    return this.repository.getProducts(this.selectedCategory);
   }
 
   get categories(): string[] {
     return this.repository.getCategories();
+  }
+
+  changeCategory(newCategory?: string) {
+    this.selectedCategory = newCategory;
   }
 }
