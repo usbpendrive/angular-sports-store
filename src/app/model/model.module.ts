5bdfa36e60593b4b19f6c214d6d3155a3e4cac4a
Fernando <ferry.fernando@gmail.com>
2018-11-02 16:15:03 +0700

    Cart model.

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { ProductRepository } from './product.repository';
 import { StaticDatasource } from './static.datasource';
+import { Cart } from './cart.model';
 
 @NgModule({
-  providers: [ProductRepository, StaticDatasource]
+  providers: [ProductRepository, StaticDatasource, Cart]
 })
 export class ModelModule {}
